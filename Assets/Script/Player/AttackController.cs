c0ac41951bd30dfedd3cc75d399d2b58f8183c65
Ilyas0011 <78249785+Ilyas0011@users.noreply.github.com>
2025-04-23 06:39:53 +0300

    update player, game entry

--- Assets/Script/Player/AttackController.cs
+++ Assets/Script/Player/AttackController.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AttackController : MonoBehaviour
 {
     [SerializeField] private Animator _handAnimator;
+    private bool isLoading = false;
+    public string cubeAddress = "CubePrefab";
+
     public void SwordSlash()
     {
         _handAnimator.SetTrigger("SwordSlash");
+
+        if(!isLoading)
+        {
+            Addressables.LoadAssetAsync<GameObject>(cubeAddress).Completed += OnCubeLoaded;
+            isLoading = true;
+            Debug.Log("Загрузка началась");
+        }
+    }
+
+    private void OnCubeLoaded(AsyncOperationHandle<GameObject> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+            Debug.Log("Загрузилос");
+        else
+            Debug.LogError("Не удалось загрузить префаб куба через Addressables.");
+
     }
 }
--- Assets/Script/SO/Config.cs
+++ Assets/Script/SO/Config.cs
@@ -20,7 +20,6 @@ public class Config : ScriptableObject
         #endif
 
         FillScreenDictionary();
-
     }
 
     private void ValidationScreenById()
